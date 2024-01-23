57df39f6e16b8da7938c2e5cc37b444449f723ed
Nick Pezza <pezza@hey.com>
2024-01-22 21:12:00 -0500

    Update schema

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_22_160406) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_22_154439) do
   create_table "active_insights_jobs", force: :cascade do |t|
     t.string "job"
     t.integer "status"
