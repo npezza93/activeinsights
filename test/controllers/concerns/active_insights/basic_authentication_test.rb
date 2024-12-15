9dcfb65bc0fccd38a29b9c5722cdf89b6edbeec0
Rob Cameron <cannikin@fastmail.com>
2024-12-15 00:02:32 +0000

    Removes test controller

--- test/controllers/concerns/active_insights/basic_authentication_test.rb
+++ test/controllers/concerns/active_insights/basic_authentication_test.rb
@@ -3,55 +3,57 @@
 require "test_helper"
 
 class ActiveInsights::BasicAuthenticationTest < ActionDispatch::IntegrationTest
-
-  class TestController < ActionController::Base
-    include ActiveInsights::BasicAuthentication
-
-    def index
-      render plain: "Hello from TestController"
-    end
-  end
-
-  setup do
-    @controller = TestController.new
-    @original_routes = Rails.application.routes
-
-    Rails.application.routes.draw do
-      get "test", to: "active_insights/basic_authentication_test/test#index"
-    end
-  end
-
-  teardown do
-    Rails.application.routes_reloader.reload!
-    ActiveInsights.http_basic_auth_enabled = nil
-    ActiveInsights.http_basic_auth_user = nil
-    ActiveInsights.http_basic_auth_password = nil
-  end
-
   test "it does not require http basic auth by default" do
-    get "/test"
+    get active_insights.requests_path
 
     assert_response :success
   end
 
   test "it requires http basic auth when enabled" do
-    ActiveInsights.http_basic_auth_enabled = true
-
-    get "/test"
-
-    assert_response :unauthorized
+    with_http_basic_auth do
+      get active_insights.requests_path
+      assert_response :unauthorized
+    end
   end
 
-  test "when enabled and proper credentials are included, it allows access" do
-    ActiveInsights.http_basic_auth_enabled = true
-    ActiveInsights.http_basic_auth_user = "johndoe"
-    ActiveInsights.http_basic_auth_password = "secret"
-    auth = ActionController::HttpAuthentication::Basic.encode_credentials(
-      "johndoe", "secret"
-    )
+  test "allows access with correct credentials" do
+    with_http_basic_auth(user: "dev", password: "secret") do
+      get active_insights.requests_path, headers: auth_headers("dev", "secret")
+      assert_response :success
+    end
+  end
 
-    get "/test", headers: { "HTTP_AUTHORIZATION" => auth }
+  test "disallows access with incorrect credentials" do
+    with_http_basic_auth(user: "dev", password: "secret") do
+      get active_insights.requests_path, headers: auth_headers("dev", "wrong")
+      assert_response :unauthorized
+    end
+  end
 
-    assert_response :success
+  private
+
+  # rubocop:disable Metrics/MethodLength
+  def with_http_basic_auth(enabled: true, user: nil, password: nil)
+    previous_enabled, ActiveInsights.http_basic_auth_enabled =
+      ActiveInsights.http_basic_auth_enabled, enabled
+    previous_user, ActiveInsights.http_basic_auth_user =
+      ActiveInsights.http_basic_auth_user, user
+    previous_password, ActiveInsights.http_basic_auth_password =
+      ActiveInsights.http_basic_auth_password, password
+    yield
+  ensure
+    ActiveInsights.http_basic_auth_enabled = previous_enabled
+    ActiveInsights.http_basic_auth_user = previous_user
+    ActiveInsights.http_basic_auth_password = previous_password
+  end
+  # rubocop:enable Metrics/MethodLength
+
+  def auth_headers(user, password)
+    {
+      Authorization:
+        ActionController::HttpAuthentication::Basic.encode_credentials(
+          user, password
+        )
+    }
   end
 end
