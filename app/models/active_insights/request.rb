b323e69a3c0a8db3a643b82503cd8a6b2451d39d
Nick Pezza <pezza@hey.com>
2025-02-28 10:52:00 -0500

    Limit user agent

--- app/models/active_insights/request.rb
+++ app/models/active_insights/request.rb
@@ -7,7 +7,8 @@ module ActiveInsights
 
       create!(started_at: started, ip_address: req.remote_ip,
               finished_at: finished, uuid: unique_id,
-              http_method: payload[:method], user_agent: req.user_agent,
+              http_method: payload[:method],
+              user_agent: req.user_agent.to_s.first(255),
               **payload.slice(:controller, :action, :format, :status,
                               :view_runtime, :db_runtime, :path))
     end
